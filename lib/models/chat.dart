ed15504ea1ad0e423a23cb4b82db82b5a8d0b28a
tr1ms <gsonly.imo@gmail.com>
2021-09-26 20:27:52 +0300

    Either Hot Shit or Total Bollocks

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -4,16 +4,16 @@ class ChatModel {
   final String title;
   final String subTitle;
   final String date;
-  final List<ChatMessage?> chatMessages;
+  List<ChatMessage> chatMessages;
 
-  const ChatModel(
+  ChatModel(
       {required this.title,
       required this.subTitle,
       required this.date,
       required this.chatMessages});
 }
 
-const demoChats = [
+var demoChats = [
   ChatModel(
       title: 'MISS CLASS OF 2016 REUNION',
       subTitle: 'Shedrack: shared a file',
@@ -24,16 +24,19 @@ const demoChats = [
           sender: 'NASA man',
           timestamp: '12:30',
           content: 't-10s to a liftoff',
+          group: 'today',
         ),
         ChatMessage(
             sender: 'james',
             type: MessageType.PlainText,
             timestamp: '12:33',
             content: 'Are they launching today?',
+            group: 'today',
             isMe: true),
         ChatMessage(
             type: MessageType.PlainText,
             sender: 'Shadrack Shine At The Hills',
+            group: 'today',
             timestamp: '12:35',
             content:
                 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio voluptate? Laboriosam voluptatum tempora sit doloremque, facere animi, nobis modi dolore dignissimos officia.'),
--- lib/models/chat_message.dart
+++ lib/models/chat_message.dart
@@ -6,32 +6,34 @@ class ChatMessage {
   final String content;
   final String timestamp;
   final bool? isMe;
+  final String group;
 
   const ChatMessage(
       {this.isMe,
       required this.type,
       required this.sender,
       required this.content,
-      required this.timestamp});
+      required this.timestamp,
+      required this.group});
 }
 
-const demoMessages = [
-  ChatMessage(
-    type: MessageType.PlainText,
-    sender: 'NASA man',
-    timestamp: '12:30',
-    content: 't-10s to a liftoff',
-  ),
-  ChatMessage(
-      sender: 'james',
-      type: MessageType.PlainText,
-      timestamp: '12:33',
-      content: 'Are they launching today?',
-      isMe: true),
-  ChatMessage(
-      type: MessageType.PlainText,
-      sender: 'Shadrack Shine At The Hills',
-      timestamp: '12:35',
-      content:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio voluptate? Laboriosam voluptatum tempora sit doloremque, facere animi, nobis modi dolore dignissimos officia.'),
-];
+// const demoMessages = [
+//   ChatMessage(
+//     type: MessageType.PlainText,
+//     sender: 'NASA man',
+//     timestamp: '12:30',
+//     content: 't-10s to a liftoff',
+//   ),
+//   ChatMessage(
+//       sender: 'james',
+//       type: MessageType.PlainText,
+//       timestamp: '12:33',
+//       content: 'Are they launching today?',
+//       isMe: true),
+//   ChatMessage(
+//       type: MessageType.PlainText,
+//       sender: 'Shadrack Shine At The Hills',
+//       timestamp: '12:35',
+//       content:
+//           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, optio voluptate? Laboriosam voluptatum tempora sit doloremque, facere animi, nobis modi dolore dignissimos officia.'),
+// ];
